206595980083619ad18e8aba0638aa62ac825a16
Pawel <aerga.pl@gmail.com>
2025-06-08 09:47:12 +0200

    dodano get

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ function subscribe(plan) {
   fetch("https://foxorox-backend.onrender.com/create-checkout-session", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ plan })
+    body: JSON.stringify({ plan: "basic_monthly" })  // lub inny
   })
-    .then(res => res.json())
-    .then(data => {
-      if (data.url) {
-        window.location.href = data.url;
-      } else {
-        alert("Nie udało się przekierować do Stripe.");
-      }
-    });
+  .then(res => res.json())
+  .then(data => {
+    if (data.url) window.location.href = data.url;
+    else alert("Błąd: brak adresu Checkout.");
+  });
 }
